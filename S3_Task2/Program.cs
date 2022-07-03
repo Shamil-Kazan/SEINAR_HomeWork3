2a7923e65345b89ea8cee9e0c962e5e45315a578
Shamil <karimshg@gmal.com>
2022-07-02 21:36:45 -0700

    add Task2

---
+++ S3_Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+void CubeNum(int num)
+{
+    int current = 1;
+
+    while(current <= num)
+    {
+        int cube = current*current*current;
+        Console.Write(cube + ", ");
+        current++;
+    }
+
+}
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+CubeNum (num);
