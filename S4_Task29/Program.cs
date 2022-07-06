ed87be3c6964036c45caa37980784fde776754ec
Shamil <karimshg@gmal.com>
2022-07-05 22:33:50 -0700

    add Task29

---
+++ S4_Task29/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+void FillArray (int [] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index<length)
+        {collection[index] = new Random().Next(1, 1000);
+        index ++;
+        }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+
+    while(position<count)
+    {
+        Console.Write(col[position] + ", ");
+        position ++;
+    }
+}
+
+Console.WriteLine("Введите длину массива");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int [num];
+FillArray(array);
+Console.Write("[");
+PrintArray(array);
+Console.Write("]");
+
