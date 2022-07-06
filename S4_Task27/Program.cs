0328f764788ec267ac462ed35c6255f0909bd018
Shamil <karimshg@gmal.com>
2022-07-05 22:30:28 -0700

    add Task27

---
+++ S4_Task27/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+int SumDigit(int num)
+ {
+     int summa = 0;
+     if (num < 0)
+     num = Math.Abs(num); //модуль числа
+     while (num > 0)
+     {   
+        int num1;
+        num1 = num % 10;
+        num = (num /10);
+        summa = summa + num1;
+     }
+     return summa;
+ }
+ Console.WriteLine("Введите число");
+ int n = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Сумма цифр числа: "+ SumDigit(n));
