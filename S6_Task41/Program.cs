5ed3bcc51e7cd0e98b50cd4888c251b5391d8022
Shamil <karimshg@gmal.com>
2022-07-14 13:19:25 -0700

    add S6Task41

---
+++ S6_Task41/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int [] CreatArray (int M)
+{
+    int [] newArray = new int [M];
+
+    Console.WriteLine("Creating array here");
+    for (int i=0; i < M; i++)
+    {
+        Console.Write($"Input {i+1} element: ");
+        newArray [i] = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.WriteLine();
+    return newArray;
+}
+
+void ShowArray (int [] array)
+{
+    for (int i=0; i < array.Length; i++)    
+        Console.Write(array [i] + " ");
+
+    Console.WriteLine();
+}
+
+Console.Write("Input size of array M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+int [] array = CreatArray (M);
+
+ShowArray (array);
+
+int count = 0; 
+M = array.Length;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]>0) count += 1;
+    }
+
+Console.WriteLine();
+Console.WriteLine(count);
