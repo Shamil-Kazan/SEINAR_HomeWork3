075038557573a639cd3929675af15acfb51efe40
Shamil <karimshg@gmal.com>
2022-07-23 18:26:22 -0700

    add S9Task64

---
+++ S9_Task64/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Задайте значение N. Напишите программу, которая найдет кол-во цифр в числе N рекурсивным методом.
+int count (int N)
+{
+    if (N/10!=0)
+        return 1+count(N/10);
+    else
+        return 1;
+}
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(count(N));
+
