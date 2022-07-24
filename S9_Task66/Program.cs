628f4ec5e0d981df696245471e7338e8c6a9bea1
Shamil <karimshg@gmal.com>
2022-07-23 18:50:49 -0700

    add S9Task66

---
+++ S9_Task66/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+ 
+int ShowNums(int m, int n)
+{
+    if(n == m) 
+        return n;
+        return m + ShowNums(m+1, n);
+}
+Console.WriteLine(ShowNums(2, 5));
+
